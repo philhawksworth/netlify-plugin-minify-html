00bdaf9654aed47bd6e83c4c44e5177e4fe4f845
Phil Hawksworth <phil@netlify.com>
2020-02-06 13:38:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const comp = require('@node-minify/core');
+const htmlMinifier = require('@node-minify/html-minifier');
+
+
+const getOptions = ({ constants, pluginConfig }) => {
+  return {
+    publishDir: constants.BUILD_DIR,
+    targetContexts: pluginConfig.targets,
+    minifierOptions: pluginConfig.minifierOptions
+  };
+};
+
+
+module.exports = {
+
+  name: 'netlify-plugin-minify-html',
+
+  onPostBuild: (args) => {
+
+    // Assemble and advertise our options
+    const {
+      publishDir,
+      targetContexts,
+      minifierOptions
+    } = getOptions(args);
+    console.log("Minifying in these deploy contexts:", targetContexts);
+    console.log("Minifying with these options:", minifierOptions);
+
+    //
+    comp({
+      compressor: htmlMinifier,
+      input: publishDir + '/**/*.html',
+      output: '$1.html',
+      replaceInPlace: true,
+      callback: function(err) {
+        if (err) console.log(err);
+      }
+    });
+
+  }
+
+}
