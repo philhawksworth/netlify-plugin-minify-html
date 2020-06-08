3e9267a51912d46d577ea50489a60b6def47d58d
Phil Hawksworth <phil@netlify.com>
2020-06-08 11:20:45 +0100

    better error handling

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const htmlMinifier = require('@node-minify/html-minifier');
 
 module.exports = {
 
-  onSuccess: ({ inputs, constants }) => {
+  onSuccess: ({ inputs, constants, utils }) => {
 
     // Only continue in the selected deploy contexts
     if( !inputs.contexts.includes(process.env.CONTEXT) ) {
@@ -15,15 +15,22 @@ module.exports = {
     // Minify HTML
     console.log('Minifiying HTML in the deploy context:', process.env.CONTEXT);
     console.log('Minifiying HTML with these options:', inputs.minifierOptions || "Default");
+
     try {
+
       comp({
         compressor: htmlMinifier,
+        // publicFolder: constants.PUBLISH_DIR,
+        // input: '**/*.html',
         input: constants.PUBLISH_DIR + '/**/*.html',
         output: '$1.html',
         replaceInPlace: true,
         options: inputs.minifierOptions
       });
+
+
     } catch (error) {
+      console.log('error :>> ', error);
       utils.build.failPlugin('The Minify HTML plugin failed.', { error })
     }
 
